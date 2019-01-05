184592ca52f106b598f4d774244940680d36a7ef
zealot <ex.arc.zhang@gmail.com>
2019-01-05 19:37:15 +0800

    fix a bug of qiniu domain

--- public/js/index/page-2-2.js
+++ public/js/index/page-2-2.js
@@ -1,8 +1,8 @@
 {
-    let view = {
-        el: 'section.hotSongs',
-        template: `        
-        <li>
+	let view = {
+		el: "section.hotSongs",
+		template: `        
+        <li onclick="window.location.href='./song.html?id={{song.id}}'">
             <div class='number'>
                 {{song.number}}
             </div>
@@ -21,57 +21,57 @@
                 </a>
             </div>
         </li>`,
-        init() {
-            this.$el = $(this.el)
-        },
-        render(data) {
-            let { songs } = data
-            for (i = 0; i < songs.length; i++) {
-                if (i < 9) {
-                    let $li = $(
-                        this.template
-                            .replace('{{song.number}}', `0${i + 1}`)
-                            .replace('{{song.name}}', songs[i].name)
-                            .replace('{{song.artist}}', songs[i].artist)
-                            .replace('{{song.id}}', songs[i].id)
-                    )
-                    this.$el.find('ol.list').append($li)
-                } else {
-                    let $li = $(
-                        this.template
-                            .replace('{{song.number}}', i + 1)
-                            .replace('{{song.name}}', songs[i].name)
-                            .replace('{{song.artist}}', songs[i].artist)
-                            .replace('{{song.id}}', songs[i].id)
-                    )
-                    this.$el.find('ol.list').append($li)
-                }
-                $('#songs-loading').remove()
-            }
-        }
-    }
-    let model = {
-        data: { songs: [] },
-        find() {
-            var query = new AV.Query('Song')
-            query.limit(20)
-            return query.find().then(songs => {
-                this.data.songs = songs.map(song => {
-                    return { id: song.id, ...song.attributes }
-                })
-                return songs
-            })
-        }
-    }
-    let controller = {
-        init(view, model) {
-            this.view = view
-            this.view.init()
-            this.model = model
-            this.model.find().then(() => {
-                this.view.render(this.model.data)
-            })
-        }
-    }
-    controller.init(view, model)
+		init() {
+			this.$el = $(this.el)
+		},
+		render(data) {
+			let { songs } = data
+			for (i = 0; i < songs.length; i++) {
+				if (i < 9) {
+					let $li = $(
+						this.template
+							.replace("{{song.number}}", `0${i + 1}`)
+							.replace("{{song.name}}", songs[i].name)
+							.replace("{{song.artist}}", songs[i].artist)
+							.replace("{{song.id}}", songs[i].id)
+					)
+					this.$el.find("ol.list").append($li)
+				} else {
+					let $li = $(
+						this.template
+							.replace("{{song.number}}", i + 1)
+							.replace("{{song.name}}", songs[i].name)
+							.replace("{{song.artist}}", songs[i].artist)
+							.replace("{{song.id}}", songs[i].id)
+					)
+					this.$el.find("ol.list").append($li)
+				}
+				$("#songs-loading").remove()
+			}
+		}
+	}
+	let model = {
+		data: { songs: [] },
+		find() {
+			var query = new AV.Query("Song")
+			query.limit(20)
+			return query.find().then(songs => {
+				this.data.songs = songs.map(song => {
+					return { id: song.id, ...song.attributes }
+				})
+				return songs
+			})
+		}
+	}
+	let controller = {
+		init(view, model) {
+			this.view = view
+			this.view.init()
+			this.model = model
+			this.model.find().then(() => {
+				this.view.render(this.model.data)
+			})
+		}
+	}
+	controller.init(view, model)
 }
--- public/js/playlist/loadMusic.js
+++ public/js/playlist/loadMusic.js
@@ -1,8 +1,8 @@
 {
-    let view = {
-        el:'main',
-        template:`   
-        <li>
+	let view = {
+		el: "main",
+		template: `   
+        <li onclick="window.location.href='./song.html?id={{song.id}}'">
         <span>{{flag}}</span>
         <div class="songMessage">
           <h3>{{song.name}}</h3>
@@ -19,44 +19,44 @@
           </a>
         </div>
         </li>`,
-        init(){
-            this.$el = $(this.el)
-        },
-        render(data){
-            let {songs} = data
-            songs.map(song=>{
-                data.flag = data.flag+1
-                let $li = $(
-                    this.template
-                        .replace('{{flag}}',data.flag)
-                        .replace('{{song.name}}', song.name)
-                        .replace('{{song.artist}}', song.artist)
-                        .replace('{{song.id}}', song.id)
-                )
-                $('#songs-loading').remove()
-                this.$el.find('ol.list').append($li)
-            })
-        }
-    }
-    let model = {
-        data:{
-            flag:0,
-            songs:[]
-        }
-    }
-    let controller = {
-        init(){
-            this.view = view
-            this.view.init()
-            this.model = model
-            this.bindEventHub()
-        },
-        bindEventHub(){
-            window.eventHub.on('getSongs',songs=>{
-                this.model.data.songs = songs
-                this.view.render(this.model.data)
-            })
-        }
-    }
-    controller.init(view,model)
+		init() {
+			this.$el = $(this.el)
+		},
+		render(data) {
+			let { songs } = data
+			songs.map(song => {
+				data.flag = data.flag + 1
+				let $li = $(
+					this.template
+						.replace("{{flag}}", data.flag)
+						.replace("{{song.name}}", song.name)
+						.replace("{{song.artist}}", song.artist)
+						.replace("{{song.id}}", song.id)
+				)
+				$("#songs-loading").remove()
+				this.$el.find("ol.list").append($li)
+			})
+		}
+	}
+	let model = {
+		data: {
+			flag: 0,
+			songs: []
+		}
+	}
+	let controller = {
+		init() {
+			this.view = view
+			this.view.init()
+			this.model = model
+			this.bindEventHub()
+		},
+		bindEventHub() {
+			window.eventHub.on("getSongs", songs => {
+				this.model.data.songs = songs
+				this.view.render(this.model.data)
+			})
+		}
+	}
+	controller.init(view, model)
 }
